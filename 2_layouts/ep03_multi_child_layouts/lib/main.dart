2ac8eadb4aac295bd544f75d2d6d1b88f4c422d5
Min Lwin <lwin.zawmin@gmail.com>
2024-10-28 00:54:57 +0630

    Layout Widgets

--- 2_layouts/ep03_multi_child_layouts/lib/main.dart
+++ 2_layouts/ep03_multi_child_layouts/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ep03_multi_child_layouts/ui/column_demo.dart';
 import 'package:ep03_multi_child_layouts/ui/flow_demo.dart';
 import 'package:ep03_multi_child_layouts/ui/grid_view_demo.dart';
 import 'package:ep03_multi_child_layouts/ui/index_stack_demo.dart';
+import 'package:ep03_multi_child_layouts/ui/layout_builder_demo.dart';
 import 'package:ep03_multi_child_layouts/ui/list_view_demo.dart';
 import 'package:ep03_multi_child_layouts/ui/row_demo.dart';
 import 'package:ep03_multi_child_layouts/ui/stack_demo.dart';
@@ -26,6 +27,7 @@ class MultiChildLayoutApp extends StatelessWidget {
         "/list": (_) => ListViewDemo(),
         "/grid": (_) => GridViewDemo(),
         "/flow": (_) => const FlowDemo(),
+        "/builder": (_) => LayoutBuilderDemo(),
       },
       initialRoute: "/",
       debugShowCheckedModeBanner: false,
@@ -43,23 +45,58 @@ class MultiChildLayoutHome extends StatelessWidget {
         title: const Text("Multi Child Layout"),
       ),
       drawer: const Sidebar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const FlutterLogo(
-              size: 200,
-            ),
-            Text(
-              "Multi Child Layout",
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-            Text(
-              "Java Developer Class",
-              style: Theme.of(context).textTheme.headlineSmall,
-            )
-          ],
-        ),
+      body: OrientationBuilder(
+        builder: (context, orientation) => orientation == Orientation.portrait
+            ? _portrait(context)
+            : _landscape(context),
+      ),
+    );
+  }
+
+  Widget _portrait(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const FlutterLogo(
+            size: 200,
+          ),
+          Text(
+            "Multi Child Layout",
+            style: Theme.of(context).textTheme.headlineLarge,
+          ),
+          Text(
+            "Java Developer Class",
+            style: Theme.of(context).textTheme.headlineSmall,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _landscape(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const FlutterLogo(
+            size: 200,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Multi Child Layout",
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              Text(
+                "Java Developer Class",
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
@@ -80,42 +117,45 @@ class Sidebar extends StatelessWidget {
             accountName: Text("Open Flutter"),
             accountEmail: Text("Java Developer Class"),
           ),
-          ListView(
-            padding: const EdgeInsets.only(top: 8, left: 8),
-            shrinkWrap: true,
-            children: [
-              ListTile(
-                title: const Text("Row"),
-                onTap: () => Navigator.of(context).pushNamed("/row"),
-              ),
-              ListTile(
-                title: const Text("Column"),
-                onTap: () => Navigator.of(context).pushNamed("/column"),
-              ),
-              ListTile(
-                title: const Text("Stack"),
-                onTap: () => Navigator.of(context).pushNamed("/stack"),
-              ),
-              ListTile(
-                title: const Text("Indexed Stack"),
-                onTap: () => Navigator.of(context).pushNamed("/indexed"),
-              ),
-              ListTile(
-                title: const Text("List View"),
-                onTap: () => Navigator.of(context).pushNamed("/list"),
-              ),
-              ListTile(
-                title: const Text("Grid View"),
-                onTap: () => Navigator.of(context).pushNamed("/grid"),
-              ),
-              ListTile(
-                title: const Text("Flow"),
-                onTap: () => Navigator.of(context).pushNamed("/flow"),
-              ),
-              const ListTile(
-                title: Text("Layout Builder"),
-              ),
-            ],
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.only(top: 8, left: 8),
+              shrinkWrap: true,
+              children: [
+                ListTile(
+                  title: const Text("Row"),
+                  onTap: () => Navigator.of(context).pushNamed("/row"),
+                ),
+                ListTile(
+                  title: const Text("Column"),
+                  onTap: () => Navigator.of(context).pushNamed("/column"),
+                ),
+                ListTile(
+                  title: const Text("Stack"),
+                  onTap: () => Navigator.of(context).pushNamed("/stack"),
+                ),
+                ListTile(
+                  title: const Text("Indexed Stack"),
+                  onTap: () => Navigator.of(context).pushNamed("/indexed"),
+                ),
+                ListTile(
+                  title: const Text("List View"),
+                  onTap: () => Navigator.of(context).pushNamed("/list"),
+                ),
+                ListTile(
+                  title: const Text("Grid View"),
+                  onTap: () => Navigator.of(context).pushNamed("/grid"),
+                ),
+                ListTile(
+                  title: const Text("Flow"),
+                  onTap: () => Navigator.of(context).pushNamed("/flow"),
+                ),
+                ListTile(
+                  title: const Text("Layout Builder"),
+                  onTap: () => Navigator.of(context).pushNamed("/builder"),
+                ),
+              ],
+            ),
           )
         ],
       ),
---
+++ 2_layouts/ep03_multi_child_layouts/lib/ui/layout_builder_demo.dart
@@ -0,0 +1,43 @@
+import 'package:ep03_multi_child_layouts/common/demo_page.dart';
+import 'package:flutter/material.dart';
+
+class LayoutBuilderDemo extends StatelessWidget {
+  final list = [
+    "https://educationalvoice.co.uk/wp-content/uploads/2024/05/Default_Careers_in_Animation_1.jpg",
+    "https://static01.nyt.com/images/2016/06/10/arts/television/coraline-watching-recommendation-LN/coraline-watching-recommendation-superJumbo.jpg",
+    "https://www.maacindia.com/_next/image?url=https%3A%2F%2Fmaacindia-public-aptech.s3.ap-south-1.amazonaws.com%2FmaacWebsite%2Fbanner%2FBanner-Bg-%25281%25291707741577537.png&w=3840&q=75",
+    "https://www.moople.in/blog/wp-content/uploads/2023/10/animation-courses-scope-03-10-2023.jpg",
+    "https://img.freepik.com/free-photo/portrait-young-student-education-day_23-2150980069.jpg",
+    "https://www.starblastanimation.com/assets/images/blog/animated-character-facial-expression.jpg"
+  ];
+  LayoutBuilderDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DemoPage(
+      title: "Layout Builder",
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return GridView.builder(
+            itemCount: list.length,
+            padding: const EdgeInsets.all(8),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount:
+                  constraints.maxWidth > constraints.maxHeight ? 3 : 2,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+            ),
+            itemBuilder: (context, index) => _getItem(index),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _getItem(int index) {
+    return Image.network(
+      fit: BoxFit.cover,
+      list[index],
+    );
+  }
+}
